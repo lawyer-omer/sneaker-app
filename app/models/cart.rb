47c4e4f03f23862df39a2ababad8a1477dceafa2
lawyer-omer <av.omeraydin29@gmail.com>
2022-09-25 16:08:54 +0300

    cart add and remove widget

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,10 @@ class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
   has_many :sneakers, through: :line_items
 
+  def sneaker_count(sneaker)
+    line_items.find_by(sneaker_id: sneaker.id)&.quantity || 0
+  end
+
   def total_price
     line_items.sum(&:total_price)
   end
