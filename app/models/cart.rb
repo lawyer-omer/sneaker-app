04aec3042a5d5fb6cffe1d702250a9c82ad93c6c
lawyer-omer <av.omeraydin29@gmail.com>
2022-09-25 15:10:36 +0300

    turbo cart added

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,4 +6,29 @@ class Cart < ApplicationRecord
   def total_price
     line_items.sum(&:total_price)
   end
+
+  def add_sneaker(sneaker)
+    item = find_item(sneaker)
+    if item
+      item.increment(:quantity)
+    else
+      item = line_items.build(sneaker_id: sneaker.id, quantity: 1)
+    end
+
+    item
+  end
+
+  def remove_sneaker(sneaker)
+    item = find_item(sneaker)
+    if item
+      item.decrement(:quantity) if item.quantity > 0
+    end
+    item.destroy if item.quantity == 0
+    item
+  end
+
+  private
+  def find_item(sneaker)
+    line_items.find_by(sneaker_id: sneaker.id)
+  end
 end
